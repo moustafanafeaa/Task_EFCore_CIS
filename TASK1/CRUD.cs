5935c9c4cb4d61201458a0219db449e2b16a412e
moustafa ahmed nafea <moustafa.anafea@gmail.com>
2024-02-23 21:33:20 +0200

    add CRUD.cs

---
+++ TASK1/CRUD.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TASK1.Models;
+using TASK1.Services;
+
+namespace TASK1
+{
+	internal static  class CRUD
+	{
+		public static void AddNewProduct(ProductService ProductService)
+		{
+			Console.Write("Product Name: ");
+			string name = Console.ReadLine();
+			Console.Write("Price: ");
+			decimal price = decimal.TryParse(Console.ReadLine(), out price) ? price : 0;
+			Console.Write("Quantity: ");
+			int quantity = int.TryParse(Console.ReadLine(), out quantity) ? quantity : 0;
+
+			Product product = new Product
+			{
+				Name = name,
+				Price = price,
+				Quantity = quantity
+			};
+			ProductService.AddProduct(product);
+			Console.WriteLine("Product now in our collections.");
+		}
+
+		public static void UpdateProduct(ProductService ProductService)
+		{
+			Console.WriteLine("Enter Product Name: ");
+
+			string productName = Console.ReadLine();
+			var product = ProductService.GetProduct(productName);
+			if (product != null)
+			{
+				Console.Write("Product Name: ");
+				string name = Console.ReadLine();
+				Console.Write("Price: ");
+				decimal price = decimal.TryParse(Console.ReadLine(), out price) ? price : 0;
+				Console.Write("Quantity: ");
+				int quantity = int.TryParse(Console.ReadLine(), out quantity) ? quantity : 0;
+				product.Name = name; product.Price = price; product.Quantity = quantity;
+
+				ProductService.Update(product);
+				Console.WriteLine("Product Updated");
+			}
+			else
+			{ Console.WriteLine("this name is wrong"); }
+
+		}
+
+		public static void DeleteProduct(ProductService ProductService)
+		{
+			Console.WriteLine("Enter Product Name: ");
+
+			string productName = Console.ReadLine();
+			var product = ProductService.GetProduct(productName);
+			if (product != null)
+			{
+				ProductService.Delete(product.Id);
+				Console.WriteLine("Product deleted now");
+			}
+		}
+
+		public static void GetAllProducts(ProductService productService)
+		{
+
+			productService.GetAllProducts();
+		}
+	}
+}
--- TASK1/Program.cs
+++ TASK1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics;
+using TASK1;
 using TASK1.Data;
 using TASK1.Models;
 using TASK1.Services;
@@ -23,81 +24,20 @@ while (true)
     string choice = Console.ReadLine();
     switch (choice)
     {
-        case "1" : AddNewProduct(productService);
+        case "1" : 
+            CRUD.AddNewProduct(productService);
             break;
-        case "2" : UpdateProduct(productService);
+        case "2" : 
+            CRUD.UpdateProduct(productService);
             break;
-        case "3" : DeleteProduct(productService);
+        case "3" :
+			CRUD.DeleteProduct(productService);
             break;
-        case "4" : GetAllProducts(productService);
+        case "4" :
+			CRUD.GetAllProducts(productService);
             break;
         case "5" : Environment.Exit(0);
             break;
         default: Console.WriteLine("Please Try again");break;
     }
-
-
 }
-
-void AddNewProduct(ProductService ProductService)
-{
-    Console.Write("Product Name: ");
-    string name = Console.ReadLine();
-    Console.Write("Price: ");
-    decimal price = decimal.TryParse(Console.ReadLine(), out price) ? price : 0;
-    Console.Write("Quantity: ");
-    int quantity = int.TryParse(Console.ReadLine(), out quantity) ? quantity : 0;
-
-    Product product = new Product
-    {
-        Name = name,
-        Price = price,
-        Quantity = quantity
-    };
-    ProductService.AddProduct(product);
-    Console.WriteLine("Product now in our collections.");
-}
-void UpdateProduct(ProductService ProductService)
-{
-    Console.WriteLine("Enter Product Name: ");
-
-    string productName = Console.ReadLine();
-    var product = ProductService.GetProduct(productName);
-    if( product != null)
-    {
-		Console.Write("Product Name: ");
-        string name = Console.ReadLine();
-		Console.Write("Price: ");
-		decimal price = decimal.TryParse(Console.ReadLine(), out price) ? price : 0;
-		Console.Write("Quantity: ");
-		int quantity = int.TryParse(Console.ReadLine(), out quantity) ? quantity : 0;
-        product.Name = name; product.Price = price; product.Quantity = quantity;
-
-        ProductService.Update(product);
-        Console.WriteLine("Product Updated");
-	}
-    else
-    { Console.WriteLine("this name is wrong"); }
-
-}
-
-void DeleteProduct(ProductService ProductService)
-{
-	Console.WriteLine("Enter Product Name: ");
-
-	string productName = Console.ReadLine();
-	var product = ProductService.GetProduct(productName);
-	if ( product != null )
-    {
-        ProductService.Delete(product.Id); 
-        Console.WriteLine("Product deleted now");
-    }
-}
-
-void GetAllProducts(ProductService productService)
-{
-    
-    productService.GetAllProducts();
-}
-
-
